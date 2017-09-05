eaf7c0d95844292231a9b2b7623cf6e502edc22a
Guillaume Alleon <guillaume.alleon@gmail.com>
2017-09-05 07:45:15 +0000

    Add autoreconf to geet rid of WARNING: 'automake-1.15' is missing on your system.

--- build.sh
+++ build.sh
@@ -48,7 +48,7 @@ echo "Leaving directory $CUDD"
 
 echo "Entering directory $MDPSIM"
 cd $MDPSIM
-./configure --prefix=$PWD --disable-shared --enable-static CFLAGS="-O3 -DNDEBUG" CXXFLAGS="-O3 -DNDEBUG"
+autoreconf -f -i;./configure --prefix=$PWD --disable-shared --enable-static CFLAGS="-O3 -DNDEBUG" CXXFLAGS="-O3 -DNDEBUG"
 make -j$(($numcpu + 1))
 make install
 cd ../..
@@ -69,7 +69,7 @@ echo "Leaving directory $MFF"
 echo "Compiling ppddl-planner"
 echo -e "\e[31mEnter the python version for which you want to compile the python wrapper ('none' if you do not want to compile the python bindings):\e[0m"
 read pythoncompatibility
-./configure --prefix=$PREFIX --with-cudd-prefix=$CUDD --with-mdpsim-prefix=$MDPSIM --with-ff-command=$(realpath ${FF}/ff) --with-mff-command=$(realpath ${MFF}/ff) CFLAGS="-O3 -DNDEBUG" CXXFLAGS="-O3 -DNDEBUG" PYTHON_VERSION="$pythoncompatibility"
+autoreconf -f -i;./configure --prefix=$PREFIX --with-cudd-prefix=$CUDD --with-mdpsim-prefix=$MDPSIM --with-ff-command=$(realpath ${FF}/ff) --with-mff-command=$(realpath ${MFF}/ff) CFLAGS="-O3 -DNDEBUG" CXXFLAGS="-O3 -DNDEBUG" PYTHON_VERSION="$pythoncompatibility"
 make -j$(($numcpu + 1))
 
 if [ "$installpolicy" == "prefix" ]; then
